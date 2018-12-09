caeddd6ac76f30ec3f1fe0391bd36479569cb42b
johnnymunster@hotmail <johnnymunster@hotmail.com>
2018-12-09 21:57:35 +0000

    added contact and about

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,17 @@ router.get('/car', function (req, res, next) {
     });
 });
 
+router.get('/about', function (req, res, next) {
+    res.render('about', {
+        title: 'about'
+    });
+});
+
+router.get('/contact', function (req, res, next) {
+    res.render('contact', {
+        title: 'contact'
+    });
+});
 
 //hiding a route
 // router.get('/', ensureAuthenticated, function (req, res, next) {
